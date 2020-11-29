9819cbf162e7d14343bcb35d59e3dcdcc2e48a53
Pavan Rao <pavanraomr@gmail.com>
2020-11-28 19:38:53 -0500

    initial commit

---
+++ main.py
@@ -0,0 +1,24 @@
+from typing import Optional
+import fastapi
+import uvicorn
+
+api = fastapi.FastAPI()
+
+@api.get('/api/calculate')
+def calculate(x:int, y:int ,z:Optional[int]=None):
+    if z == 0:
+        return fastapi.Response(content='{"error":"Error: Z cannot be 0"}',
+                                 media_type="application/json",
+                                 status_code=400)
+    value = x + y
+    if z is not None:
+        value /= z
+    return {
+        'x': x,
+        'y': y,
+        'z': z,
+        'value':value
+    }
+
+uvicorn.run(api, port=8000, host = "127.0.0.1")
+
